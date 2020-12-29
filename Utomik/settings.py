b2fc4b0801adde544a7f8c276fe85d04e4d2d455
Omendra Manhar <omendra.manhar@gmail.com>
2020-12-29 18:17:56 +0100

    Update settings.py

--- Utomik/settings.py
+++ Utomik/settings.py
@@ -18,11 +18,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^kdyniqm@dl9$y^+eaf(7=_-3v*ty&i6$5g_qr!f=5l+5kt=^n'
+SECRET_KEY = '<key>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
